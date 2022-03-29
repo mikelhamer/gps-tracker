a919b3da4f6f50f7f48859ea7b07bf561e7ee5ea
mikelhamer <mikelhamer@gmail.com>
2022-03-28 22:02:31 -0400

    Display current position on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ViewWillEnter } from '@ionic/angular';
+import { Geolocation, Position } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements ViewWillEnter {
 
-  constructor() {}
+  currentPosition: Position;
+
+  constructor() {
+  }
+
+  async ionViewWillEnter() {
+    this.currentPosition = await Geolocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 5000
+    });
+    console.log('Position: ' + JSON.stringify(this.currentPosition));
+  }
 
 }
